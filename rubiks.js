8ad35177122360677a22ed66fa75df2d373b8f46
Miguel Zavala <mikeyrocks16@gmail.com>
2024-07-31 15:49:02 -0700

    Version 1.4
    - vertical layers on z-axis now have rotation animations

--- rubiks.js
+++ rubiks.js
@@ -4,59 +4,69 @@ let blue = [];
 let green = [];
 let white = [];
 let orange = [];
-frontDegree = 90;
-backDegree = 90;
-leftSideDegree = 90;
-rightSideDegree = 90;
-midVertDegree = 90;
-midVert2Degree = 90;
-midHorDegree = 90;
-bottomDegree = 90;
-topDegree = 90;
+frontDegree = 0;
+midVertDegree = 0;
+backDegree = 0;
+leftSideDegree = 0;
+rightSideDegree = 0;
+midVert2Degree = 0;
+midHorDegree = 0;
+bottomDegree = 0;
+topDegree = 0;
 
 
-function rotateFront() {
-    // let transformations = {
-    //     transform: "rotate3d(0,0,1, 0deg) translate3d(0,0,100px)",
-    //     transform: "rotate3d(0,0,1, 90deg) translate3d(0,0,100px)",
-    // }
-    // let keyframesTiming = {duration: 1000}
-    // front.animate(transformations, keyframesTiming);
+function rotateFront() { 
     let front = document.querySelector(".side-front");
-    front.style.transform = "rotate3d(0,0,1," +frontDegree+ "deg) translate3d(0,0,100px)";
-    if (frontDegree >= 360) {
-        frontDegree = 90;
-    } else {
-        frontDegree += 90;
+    let transformations = {
+        transform: "rotate3d(0,0,1, " +(frontDegree)+ "deg) translate3d(0,0,100px)",
+        transform: "rotate3d(0,0,1, " +(frontDegree+90)+ "deg) translate3d(0,0,100px)",
     }
-    // setTimeout(()=>{
-    //     front.style.transform = "rotate3d(0,0,1, 90deg) translate3d(0,0,100px)";
-    //     if (degree > 360) {
-    //         degree = 0;
-    //     } else {
-    //         degree += 90;
-    //     }
-    // }, 1500)
+    let keyframesTiming = {
+        duration: 500,
+        iterations: 1,
+    }
+    front.animate(transformations, keyframesTiming);
+    setTimeout(()=>{
+        front.style.transform = "rotate3d(0,0,1, " +(frontDegree)+ "deg) translate3d(0,0,100px)";
+        front.style.transform = "rotate3d(0,0,1, " +(frontDegree+90)+ "deg) translate3d(0,0,100px)";
+        frontDegree += 90;
+    }, 500)
 };
 
 function rotateMiddleVertical() {
     let midVert = document.querySelector(".side-middle");
-    midVert.style.transform = "rotate3d(0,0,1," +midVertDegree+ "deg)";
-    if (midVertDegree >= 360) {
-        midVertDegree = 90;
-    } else {
-        midVertDegree += 90;
+    let transformations = {
+        transform: "rotate3d(0,0,1, " +(midVertDegree)+ "deg)",
+        transform: "rotate3d(0,0,1, " +(midVertDegree+90)+ "deg)",
     }
+    let keyframesTiming = {
+        duration: 500,
+        iterations: 1,
+    }
+    midVert.animate(transformations, keyframesTiming);
+    setTimeout(()=>{
+        midVert.style.transform = "rotate3d(0,0,1, " +(midVertDegree)+ "deg)";
+        midVert.style.transform = "rotate3d(0,0,1, " +(midVertDegree+90)+ "deg)";
+        midVertDegree += 90;
+    }, 500)
 };
 
 function rotateBack() {
     let back = document.querySelector(".side-back");
-    back.style.transform = "rotate3d(0,0,1," +backDegree+ "deg) translate3d(0,0,-100px)";
-    if (backDegree >= 360) {
-        backDegree = 90;
-    } else {
-        backDegree += 90;
+    let transformations = {
+        transform: "rotate3d(0,0,1, " +(backDegree)+ "deg) translate3d(0,0,-100px)",
+        transform: "rotate3d(0,0,1, " +(backDegree+90)+ "deg) translate3d(0,0,-100px)",
     }
+    let keyframesTiming = {
+        duration: 500,
+        iterations: 1,
+    }
+    back.animate(transformations, keyframesTiming);
+    setTimeout(()=>{
+        back.style.transform = "rotate3d(0,0,1, " +(backDegree)+ "deg) translate3d(0,0,-100px)";
+        back.style.transform = "rotate3d(0,0,1, " +(backDegree+90)+ "deg) translate3d(0,0,-100px)";
+        backDegree += 90;
+    }, 500)
 };
 
 function rotateMiddleHorizontal() {
@@ -131,13 +141,15 @@ function rotateRightSide() {
     }
 };
 
-setInterval(rotateTop, 200);
-setInterval(rotateBottom, 250);
-setInterval(rotateFront, 350);
-setInterval(rotateBack, 400);
-setInterval(rotateMiddleVertical2, 450);
-setInterval(rotateLeftSide, 500);
-setInterval(rotateRightSide, 550);
-setInterval(rotateMiddleHorizontal, 600);
+
+// setInterval(rotateTop, 100);
+// setInterval(rotateMiddleHorizontal, 125);
+// setInterval(rotateBottom, 150);
+// setInterval(rotateFront, 1000);
+setInterval(rotateMiddleVertical, 1000);
+// setInterval(rotateBack, 1000);
+// setInterval(rotateLeftSide, 250);
+// setInterval(rotateMiddleVertical2, 275);
+// setInterval(rotateRightSide, 300);
 
 
