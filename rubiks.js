46bc018c2577608d165e2f2c1f1080207630689d
Miguel Zavala <mikeyrocks16@gmail.com>
2024-08-08 12:19:36 -0700

    Version 1.5
    - cube rotations around the y-axis play a spinning animation (doesn't stick) dependent on the side being turned by adding and removing animation classes. Actual transformation rotations (do stick) are then made to the cube after the animations. So far this is only implemented in the top layer.

--- rubiks.js
+++ rubiks.js
@@ -4,15 +4,15 @@ let blue = [];
 let green = [];
 let white = [];
 let orange = [];
-frontDegree = 0;
-midVertDegree = 0;
-backDegree = 0;
-leftSideDegree = 0;
-rightSideDegree = 0;
-midVert2Degree = 0;
-midHorDegree = 0;
-bottomDegree = 0;
-topDegree = 0;
+let frontDegree = 0;
+let midVertDegree = 0;
+let backDegree = 0;
+let leftSideDegree = 0;
+let midVert2Degree = 0;
+let rightSideDegree = 0;
+let topDegree = 0;
+let midHorDegree = 0;
+let bottomDegree = 0;
 
 
 function rotateFront() { 
@@ -26,11 +26,10 @@ function rotateFront() {
         iterations: 1,
     }
     front.animate(transformations, keyframesTiming);
-    setTimeout(()=>{
-        front.style.transform = "rotate3d(0,0,1, " +(frontDegree)+ "deg) translate3d(0,0,100px)";
-        front.style.transform = "rotate3d(0,0,1, " +(frontDegree+90)+ "deg) translate3d(0,0,100px)";
-        frontDegree += 90;
-    }, 500)
+    // setTimeout(()=>{
+    //     front.style.transform = "rotate3d(0,0,1, " +(frontDegree)+ "deg) translate3d(0,0,100px)";
+    //     front.style.transform = "rotate3d(0,0,1, " +(frontDegree+90)+ "deg) translate3d(0,0,100px)";
+    // }, 500)
 };
 
 function rotateMiddleVertical() {
@@ -70,51 +69,140 @@ function rotateBack() {
 };
 
 function rotateMiddleHorizontal() {
-    let midHor = document.getElementsByClassName("mid-layer");
+    let midHor = document.querySelectorAll(".mid-layer");
     for (let i = 0; i < midHor.length; i++) {
-        midHor[i].style.transform = "rotate3d(0,1,0," +midHorDegree+ "deg)";
+        let transformations = {
+            transform: "rotate3d(0,1,0, " +(midHorDegree)+ "deg)",
+            transform: "rotate3d(0,1,0, " +(midHorDegree+90)+ "deg)",
+        }
+        let keyframesTiming = {
+            duration: 500,
+            iterations: 1,
+        }
+        midHor[i].animate(transformations, keyframesTiming);
     }
-    if (midHorDegree >= 360) {
-        midHorDegree = 90;
-    } else {
-        midHorDegree += 90;
-    }
+    // setTimeout(()=>{
+    //     for (let i = 0; i < midHor.length; i++) {
+    //         midHor[i].style.transform = "rotate3d(0,1,0, " +(midHorDegree)+ "deg)";
+    //         midHor[i].style.transform = "rotate3d(0,1,0, " +(midHorDegree+90)+ "deg)";
+    //     }
+    //     midHorDegree += 90;
+    // }, 500)
 };
 
 function rotateTop() {
-    let top = document.getElementsByClassName("top-layer");
-    for (let i = 0; i < top.length; i++) {
-        top[i].style.transform = "rotate3d(0,1,0," +topDegree+ "deg)";
+    let top = document.querySelectorAll(".top-layer");
+
+    // side-front
+    // let leftCornerPos = document.querySelector(".left-corner-z-pos");
+    top[0].style.transformOrigin = "150px center -100px";
+
+    // let middleEdgePos = document.querySelector(".middle-edge-z-pos");
+    top[1].style.transformOrigin = "center center -100px";
+
+    // let rightCornerPos = document.querySelector(".right-corner-z-pos");
+    top[2].style.transformOrigin = "-50px center -100px";
+
+    //side-middle
+    // let leftEdge = document.querySelector(".left-edge-z");
+    top[3].style.transformOrigin = "150px center 0px";
+
+    // let rightEdge = document.querySelector(".right-edge-z");
+    top[5].style.transformOrigin = "-50px center 0px";
+
+    //side-back
+    // let leftCornerNeg = document.querySelector(".left-corner-z-neg");
+    top[6].style.transformOrigin = "150px center 100px";
+
+    // let middleEdgeNeg = document.querySelector(".middle-edge-z-neg");
+    top[7].style.transformOrigin = "center center 100px";
+
+    // let rightCornerNeg = document.querySelector(".right-corner-z-neg");
+    top[8].style.transformOrigin = "-50px center 100px";
+    
+    //adds spinning animation dependent the side being turned
+    if (topDegree == 0) {
+        for (let i = 0; i < top.length; i++) {
+            top[i].classList.add("spin90-y-axis");
+        }
+    } else if (topDegree == 90) {
+        for (let i = 0; i < top.length; i++) {
+            top[i].classList.add("spin180-y-axis");
+        }
+    } else if (topDegree == 180) {
+        for (let i = 0; i < top.length; i++) {
+            top[i].classList.add("spin270-y-axis");
+        }
+    } else if (topDegree == 270) {
+        for (let i = 0; i < top.length; i++) {
+            top[i].classList.add("spin360-y-axis");
+        }
     }
-    if (topDegree >= 360) {
-        topDegree = 90;
-    } else {
-        topDegree += 90;
-    }
+
+    //waits for animation to play before spinning the individuals cubes
+    setTimeout(()=>{
+        for (let i = 0; i < top.length; i++) {
+            top[i].classList.remove("spin90-y-axis");
+            top[i].classList.remove("spin180-y-axis");
+            top[i].classList.remove("spin270-y-axis");
+            top[i].classList.remove("spin360-y-axis");
+            top[i].style.transformOrigin = "center";
+        }
+        for (let i = 0; i < top.length; i++) {
+            top[i].style.transform = "rotate3d(0,1,0, " +(topDegree)+ "deg)";
+            top[i].style.transform = "rotate3d(0,1,0, " +(topDegree+90)+ "deg)";
+        }
+        if (topDegree == 270) {
+            topDegree = 0;
+        } else {
+            topDegree += 90;
+        }
+    }, 500)
+
 };
 
 function rotateBottom() {
-    let bottom = document.getElementsByClassName("bottom-layer");
+    let bottom = document.querySelectorAll(".bottom-layer");
     for (let i = 0; i < bottom.length; i++) {
-        bottom[i].style.transform = "rotate3d(0,1,0," +bottomDegree+ "deg)";
+        let transformations = {
+            transform: "rotate3d(0,1,0, " +(bottomDegree)+ "deg)",
+            transform: "rotate3d(0,1,0, " +(bottomDegree+90)+ "deg)",
+        }
+        let keyframesTiming = {
+            duration: 500,
+            iterations: 1,
+        }
+        bottom[i].animate(transformations, keyframesTiming);
     }
-    if (bottomDegree >= 360) {
-        bottomDegree = 90;
-    } else {
+    setTimeout(()=>{
+        for (let i = 0; i < bottom.length; i++) {
+            bottom[i].style.transform = "rotate3d(0,1,0, " +(bottomDegree)+ "deg)";
+            bottom[i].style.transform = "rotate3d(0,1,0, " +(bottomDegree+90)+ "deg)";
+        }
         bottomDegree += 90;
-    }
+    }, 500)
 };
 
 function rotateLeftSide() {
-    let leftSide = document.getElementsByClassName("left-side");
+    let leftSide = document.querySelectorAll(".left-side");
     for (let i = 0; i < leftSide.length; i++) {
-        leftSide[i].style.transform = "rotate3d(1,0,0," +leftSideDegree+ "deg)";
+        let transformations = {
+            transform: "rotate3d(1,0,0, " +(leftSideDegree)+ "deg)",
+            transform: "rotate3d(1,0,0, " +(leftSideDegree+90)+ "deg)",
+        }
+        let keyframesTiming = {
+            duration: 500,
+            iterations: 1,
+        }
+        leftSide[i].animate(transformations, keyframesTiming);
     }
-    if (leftSideDegree >= 360) {
-        leftSideDegree = 90;
-    } else {
+    setTimeout(()=>{
+        for (let i = 0; i < leftSide.length; i++) {
+            leftSide[i].style.transform = "rotate3d(1,0,0, " +(leftSideDegree)+ "deg)";
+            leftSide[i].style.transform = "rotate3d(1,0,0, " +(leftSideDegree+90)+ "deg)";
+        }
         leftSideDegree += 90;
-    }
+    }, 500)
 }
 
 function rotateMiddleVertical2() {
@@ -141,14 +229,15 @@ function rotateRightSide() {
     }
 };
 
-
-// setInterval(rotateTop, 100);
-// setInterval(rotateMiddleHorizontal, 125);
-// setInterval(rotateBottom, 150);
-// setInterval(rotateFront, 1000);
-setInterval(rotateMiddleVertical, 1000);
+// rotateTop();
+// rotateFront();
+setInterval(rotateTop, 1000);
+// setInterval(rotateMiddleHorizontal, 1000);
+// setInterval(rotateBottom, 1000);
+// setInterval(rotateFront, 1500);
+// setInterval(rotateMiddleVertical, 1000);
 // setInterval(rotateBack, 1000);
-// setInterval(rotateLeftSide, 250);
+// setInterval(rotateLeftSide, 1000);
 // setInterval(rotateMiddleVertical2, 275);
 // setInterval(rotateRightSide, 300);
 
